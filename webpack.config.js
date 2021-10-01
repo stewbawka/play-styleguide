dcd5324553eda8e5a421cc87271ef2cb27cd61d6
Stuart Wade <stewbawka@gmail.com>
2021-10-01 19:31:25 -0300

    initial commit of style guide /w tailwindcss

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = webpackConfigEnv => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "discostudebaker",
+    projectName: "styleguide",
+    webpackConfigEnv
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            //"style-loader",
+            //"css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  ident: "postcss",
+                  plugins: [require("tailwindcss"), require("autoprefixer")]
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  });
+};
